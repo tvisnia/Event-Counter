14bdb69e9d81d51e7587a5701e46d62fc5e8821c
Tomek Wiśniewski <tomasz.a.wisniewski@comarch.com>
2019-07-03 11:05:18 +0200

    +action and reducers rearranged

--- redux/actions/selectionModeActions.js
+++ redux/actions/selectionModeActions.js
@@ -1,6 +1,6 @@
 import {TOGGLE_SELECTION_MODE} from './types'
 
-export const turnSelectionModeOn = () => {
+export const toggleEventSelectionMode = () => {
     return {
         type: TOGGLE_SELECTION_MODE
     }
--- redux/reducers/selectionModeReducer.js
+++ redux/reducers/selectionModeReducer.js
@@ -1,10 +1,10 @@
-import { TURN_SELECTION_MODE_ON} from '../actions/types'
+import { TOGGLE_SELECTION_MODE} from '../actions/types'
 
 const initialState = false
 
 export default(state = initialState, action) => {
     switch(action.type) {
-        case TURN_SELECTION_MODE_ON:
+        case TOGGLE_SELECTION_MODE:
             return !state
         default:
             return state
